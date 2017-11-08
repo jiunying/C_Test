bc64fd76b28ef7c31f38658d06b82243635130ee
JiunYing Yeh <wink00900600@gmail.com>
2017-11-08 22:48:49 +0800

    * modify Add_Difits more faster

--- Add_Digits/main.c
+++ Add_Digits/main.c
@@ -1,34 +1,9 @@
 #include <stdio.h>
 
 
-int addDigits(int num)
+static int addDigits(int num)
 {
-    int index = 0, i;
-    int quoitent = 0, remainder = 0;
-    int buf[256] = {0};
-    int sum = num;
-    do
-    {
-        quoitent = sum;
-        for(i = 0; i < index; ++i)
-        {
-            buf[i] = 0;
-        }
-        index = 0;
-        sum = 0;
-        do
-        {
-            remainder = quoitent%10;
-            buf[index] = remainder;
-            index++;
-            quoitent /= 10;
-        }while(quoitent);
-        for(i = 0; i < index; ++i)
-        {
-            sum += buf[i];
-        }
-    }while(sum >= 10);
-    return sum;
+    return (num - 1)%9 + 1;
 }
 
 int main()
